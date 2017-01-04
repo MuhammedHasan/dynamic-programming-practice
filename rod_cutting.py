f32b717063c29780a118512a8389e8b18e03688c
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-01-04 19:37:29 +0300

    init

---
+++ rod_cutting.py
@@ -0,0 +1,51 @@
+import math
+
+
+class RodCutter(object):
+
+    def __init__(self, prices):
+        self.prices = prices
+        self.memory = dict()
+
+    def cut_road(self, road_length):
+        if road_length == 0:
+            return 0
+        profit = -math.inf
+        for i, v in enumerate(self.prices):
+            i += 1
+            if road_length >= i:
+                t_profit = v + self.cut_road(road_length - i)
+                profit = max(profit, t_profit)
+        return profit
+
+    def memorized_cut_rod(self, road_length):
+        self.memory = dict()
+        return self.memorized_cut_rod_x(road_length)
+
+    def memorized_cut_rod_x(self, road_length):
+        if self.memory.get(road_length):
+            return self.memory[road_length]
+        if road_length == 0:
+            profit = 0
+        else:
+            profit = -math.inf
+            for i, v in enumerate(self.prices):
+                i += 1
+                if road_length >= i:
+                    t_profit = v + self.memorized_cut_rod_x(road_length - i)
+                    profit = max(profit, t_profit)
+        self.memory[road_length] = profit
+        return profit
+
+    def bottom_up_cut_rod(self, road_length):
+        memory = {0: 0}
+        for j in range(1, road_length + 1):
+            profit = -math.inf
+            for i, p in enumerate(self.prices):
+                i += 1
+                if j >= i:
+                    t_profit = p + memory[j - i]
+                    profit = max(profit, t_profit)
+            memory[j] = profit
+
+        return memory[road_length]
---
+++ tests.py
@@ -0,0 +1,42 @@
+import unittest
+
+from rod_cutting import RodCutter
+
+
+class TestsRodCutter(unittest.TestCase):
+
+    def setUp(self):
+        prices = [1,  5, 8,  9, 10, 17, 17, 20, 24, 30]
+        self.cutter = RodCutter(prices)
+
+    def based_rod_cutting(self, cutter_function):
+        self.assertEqual(cutter_function(1), 1)
+        self.assertEqual(cutter_function(2), 5)
+        self.assertEqual(cutter_function(3), 8)
+        self.assertEqual(cutter_function(4), 10)
+        self.assertEqual(cutter_function(5), 13)
+        self.assertEqual(cutter_function(6), 17)
+        self.assertEqual(cutter_function(7), 18)
+        self.assertEqual(cutter_function(8), 22)
+        self.assertEqual(cutter_function(9), 25)
+        self.assertEqual(cutter_function(10), 30)
+        self.assertEqual(cutter_function(11), 31)
+
+    def based_cut_road(self, cutter_function):
+        for i in range(10, 20):
+            self.assertEqual(self.cutter.cut_road(i), cutter_function(i))
+
+    def test_cut_road(self):
+        self.based_rod_cutting(self.cutter.cut_road)
+
+    def test_memorized_cut_rod(self):
+        self.based_rod_cutting(self.cutter.memorized_cut_rod)
+        self.based_cut_road(self.cutter.memorized_cut_rod)
+
+    def test_bottom_up_cut_rod(self):
+        self.based_rod_cutting(self.cutter.bottom_up_cut_rod)
+        self.based_cut_road(self.cutter.bottom_up_cut_rod)
+
+
+if __name__ == "__main__":
+    unittest.main()
